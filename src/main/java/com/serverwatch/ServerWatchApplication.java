b2611e638086104d11470cd2db18901032e331fb
alperenklc5 <kilicalperen80@gmail.com>
2026-07-07 10:43:08 +0300

    Exclude PrePostMethodSecurityConfiguration to fix WebSocket terminal

--- src/main/java/com/serverwatch/ServerWatchApplication.java
+++ src/main/java/com/serverwatch/ServerWatchApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springframework.security.config.annotation.method.configuration.PrePostMethodSecurityConfiguration.class
+})
 @EnableScheduling
 @EnableConfigurationProperties(ServerWatchProperties.class)
 public class ServerWatchApplication {
